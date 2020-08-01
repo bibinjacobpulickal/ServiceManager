754d51bec713520b7d5e9ee6af6a0ba776fb5eb7
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-08-02 02:22:59 +0530

    Updated HTTPScheme.

--- Sources/ServiceManager/HTTP/HTTPScheme.swift
+++ Sources/ServiceManager/HTTP/HTTPScheme.swift
@@ -1,13 +1,34 @@
 //
 //  HTTPScheme.swift
-//  ServiceManager
 //
-//  Created by Bibin Jacob Pulickal on 12/07/19.
-//  Copyright © 2019 Bibin Jacob Pulickal. All rights reserved.
+//  Copyright © 2020 Bibin Jacob Pulickal (https://github.com/bibinjacobpulickal)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
-import Foundation
+public struct HTTPScheme {
+    /// `HTTPS` scheme.
+    public static let https = HTTPScheme(rawValue: "https")
+    /// `HTTP` scheme.
+    public static let http  = HTTPScheme(rawValue: "http")
+    /// `FTP` scheme.
+    public static let ftp   = HTTPScheme(rawValue: "ftp")
 
-public enum HTTPScheme: String, CaseIterable {
-    case http, https, ftp
+    public let rawValue: String
 }
