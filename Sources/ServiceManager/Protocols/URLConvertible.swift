5b080e145377fa511d49f485204bf61d851850b5
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-08-01 16:37:02 +0530

    Updated URL and URLRequest convertible protocol definition.

--- Sources/ServiceManager/Protocols/URLConvertible.swift
+++ Sources/ServiceManager/Protocols/URLConvertible.swift
@@ -1,43 +1,85 @@
 //
-//  URLConvertible.swift
-//  ServiceManager
+//  URLConvertible+URLRequestConvertible.swift
 //
-//  Created by Bibin Jacob Pulickal on 11/07/19.
-//  Copyright © 2019 Bibin Jacob Pulickal. All rights reserved.
+//  Copyright © 2020 Bibin Jacob Pulickal (https://github.com/bibinjacobpulickal)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 
-public protocol URLConvertible: RequestConvertible {
+/// Types adopting the `URLConvertible` protocol can be used to construct `URL`s, which can then be used to construct
+/// `URLRequests`.
+public protocol URLConvertible {
+    /// Returns a `URL` from the conforming instance or throws.
+    ///
+    /// - Returns: The `URL` created from the instance.
+    /// - Throws:  Any error thrown while creating the `URL`.
     func asURL() throws -> URL
 }
 
-public extension URLConvertible {
+extension String: URLConvertible {
+    /// Returns a `URL` if `self` can be used to initialize a `URL` instance, otherwise throws.
+    ///
+    /// - Returns: The `URL` initialized with `self`.
+    /// - Throws:  An `AFError.invalidURL` instance.
+    public func asURL() throws -> URL {
+        guard let url = URL(string: self) else { throw HTTPError.invalidURL(url: self) }
 
-    func asRequest() throws -> URLRequest {
-        try URLRequest(url: asURL())
+        return url
     }
 }
 
 extension URL: URLConvertible {
+    /// Returns `self`.
     public func asURL() throws -> URL { self }
 }
 
-extension String: URLConvertible {
-
+extension URLComponents: URLConvertible {
+    /// Returns a `URL` if the `self`'s `url` is not nil, otherwise throws.
+    ///
+    /// - Returns: The `URL` from the `url` property.
+    /// - Throws:  An `AFError.invalidURL` instance.
     public func asURL() throws -> URL {
-        if let url = URL(string: self) {
-            return url
-        } else {
-            throw HTTPError.invalidURL(url: self)
-        }
+        guard let url = url else { throw HTTPError.invalidURL(url: self) }
+
+        return url
     }
 }
 
-extension URLComponents: URLConvertible {
+// MARK: -
 
-    public func asURL() throws -> URL {
-        guard let url = url else { throw HTTPError.invalidURL(url: self) }
-        return url
-    }
+/// Types adopting the `URLRequestConvertible` protocol can be used to safely construct `URLRequest`s.
+public protocol URLRequestConvertible {
+    /// Returns a `URLRequest` or throws if an `Error` was encountered.
+    ///
+    /// - Returns: A `URLRequest`.
+    /// - Throws:  Any error thrown while constructing the `URLRequest`.
+    func asURLRequest() throws -> URLRequest
+}
+
+extension URLRequestConvertible {
+    /// The `URLRequest` returned by discarding any `Error` encountered.
+    public var urlRequest: URLRequest? { try? asURLRequest() }
+}
+
+extension URLRequest: URLRequestConvertible {
+    /// Returns `self`.
+    public func asURLRequest() throws -> URLRequest { self }
 }
